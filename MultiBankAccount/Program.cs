afa6cfcc47f57d17f9cf470461fc3262f51285a1
isa-sanchez-vuel <isabela.sanchez@col.vueling.com>
2024-10-24 11:28:20 +0200

    Some fixes and decoration
    - polished output design
    - improved income and outcome input filters
    - created constant variables MAX_INCOME, MAX_OUTCOME and EXIT_NUMBER

--- MultiBankAccount/Program.cs
+++ MultiBankAccount/Program.cs
@@ -29,7 +29,9 @@ List<string> pinList = new List<string>
 #endregion
 
 #region TRANSACTIONS
-decimal maxMoneyTransaction = 6000;
+const decimal MAX_INCOME = 5000;
+const decimal MAX_OUTCOME = 2000;
+const int EXIT_NUMBER = 7;
 string? input;
 int optionInt;
 bool exit = false;
@@ -63,6 +65,7 @@ Console.OutputEncoding = System.Text.Encoding.UTF8;
 do {
     Console.WriteLine("Account number (XXXXXXXXXX): ");
 
+    Console.Write("=> ");
     input = Console.ReadLine()?.Trim();
 
     foreach (string account in accountList)
@@ -78,6 +81,7 @@ do {
 
     Console.WriteLine("Pin (XXXX): ");
 
+    Console.Write("=> ");
     input = Console.ReadLine()?.Trim();
 
     foreach (string pin in pinList)
@@ -98,22 +102,17 @@ do {
     if (pinIndex == accountIndex)
     {
         positionCorrect = true;
+        currentUser = pinIndex; //Assign current user index
     }
     else positionCorrect = false;
 
-    if(!userExists || !positionCorrect)
-    {
-        Console.WriteLine("Insert valid account number and pin:");
-    }
+    if(!userExists || !positionCorrect) Console.WriteLine("Insert valid account number and pin:");
 
 } while (!userExists || !positionCorrect);
 
-
-currentUser = pinIndex;
-
+//Assign current user money and movements
 List<string> movementList = accountsMovementList[currentUser];
 currentMoney = accountsMoneyList[currentUser];
-
 #endregion
 
 #region ACCOUNT MOVEMENTS
@@ -121,17 +120,22 @@ do
 {
     Console.Clear();
 
-    Console.WriteLine(@"Welcome to your bank account! What do you want to do?
+    Console.WriteLine($@"
+==============================================================
+|| Welcome to your bank account! What do you want to do?    ||
+||                                                          ||
+||	1 - Money Income                                    ||
+||	2 - Money Outcome                                   ||
+||	3 - List all movements                              ||
+||	4 - List Incomes                                    ||
+||	5 - List Outcomes                                   ||
+||	6 - Show current money                              ||
+||	{EXIT_NUMBER} - Exit                                            ||
+||                                                          ||
+|| Please write the option's number:                        ||
+==============================================================");
 
-	1 - Money Income
-	2 - Money Outcome
-	3 - List all movements
-	4 - List Incomes
-	5 - List Outcomes
-	6 - Show current money
-	7 - Exit" +
-    "\n\nPlease write the option's number: ");
-
+    Console.Write("=> ");
     input = Console.ReadLine()?.Trim();
 
     if (input != null && input.Length == 1 && input[0] >= '1' && input[0] <= '7' && int.TryParse(input, out optionInt))
@@ -140,45 +144,48 @@ do
         Console.WriteLine("Option chosen: " + optionInt);
 
 
-        if (optionInt != 7)
+        if (optionInt != EXIT_NUMBER)
         {
             switch (optionInt)
             {
                 case 1:
-                    Console.WriteLine("Please write how much money you want to deposit:");
-
+                    Console.WriteLine("=================================================" +
+                        "Please write how much money you want to deposit:");
                     decimal income = 0;
 
                     do
                     {
-                        input = Console.ReadLine()?.Trim();
+                        Console.Write("=> ");
+                        input = Console.ReadLine()?.Trim().Replace(".", ",");
 
                         if (input != null && input.Length > 0 && decimal.TryParse(input, out income))
                         {
-                            income = Math.Abs(decimal.Parse(input));
-
-                            if (income <= maxMoneyTransaction)
+                            if (income <= MAX_INCOME)
                             {
                                 currentMoney += income;
                                 movementList.Add("+" + income + " | " + DateTime.Now);
-                                Console.WriteLine(income + " $ were added to your account.");
+                                Console.WriteLine($"{income}€ were added to your account.\n" +
+                                    $"Current money: {currentMoney:0.00}€");
                             }
-                            else Console.WriteLine($"ERROR: The top amount you can deposit is {maxMoneyTransaction}. Try again.");
+                            else Console.WriteLine($"ERROR: The top amount you can deposit is {MAX_INCOME:0.00}€. Try again.");
                         }
                         else
                         {
-                            Console.WriteLine("ERROR: Please write a numeric value: ");
+                            Console.WriteLine(
+                                "ERROR: Please write a numeric value: ");
                         }
-                    } while (!decimal.TryParse(input, out income) || income > maxMoneyTransaction);
+                    } while (!decimal.TryParse(input, out income) || income > MAX_INCOME);
                     break;
 
                 case 2:
                     decimal outcome = 0;
-                    Console.WriteLine("Please write how much money you want to withdraw:");
+                    Console.WriteLine("=================================================" +
+                        "Please write how much money you want to withdraw:");
 
                     do
                     {
-                        input = Console.ReadLine()?.Trim();
+                        Console.Write("=> ");
+                        input = Console.ReadLine()?.Trim().Replace(".", ",");
 
                         if (input != null && input.Length > 0 && decimal.TryParse(input, out outcome))
                         {
@@ -188,13 +195,14 @@ do
                             {
                                 currentMoney -= outcome;
                                 movementList.Add("-" + outcome + " | " + DateTime.Now);
-                                Console.WriteLine(outcome + " $ were subtracted from your account.");
+                                Console.WriteLine($"{outcome:0.00}€ were subtracted from account.\n" +
+                                    $"Current money: {currentMoney:0.00}€");
                             }
                             else
                             {
                                 Console.WriteLine(
                                     "ERROR: You cannot withdraw this amount of money. Try a lower value.\n" +
-                                    $"Money available: {currentMoney:0.00} €\n" +
+                                    $"Money available: {currentMoney:0.00}€\n" +
                                     "Amount to withdraw: ");
                             }
                         }
@@ -203,49 +211,56 @@ do
                             Console.WriteLine(
                                 "ERROR: Please write a numeric value: ");
                         }
-                    } while (outcome > currentMoney || !decimal.TryParse(input, out outcome));
+                    } while (!decimal.TryParse(input, out outcome) || outcome > MAX_OUTCOME || outcome > currentMoney);
 
                     break;
 
                 case 3:
-                    Console.WriteLine("These are all your movements:\n");
+                    Console.WriteLine("=================================================\n" +
+                        "These are all your movements:\n" +
+                        "-------------------------------------------------");
 
                     foreach (string movement in movementList)
                     {
                         Console.WriteLine(movement);
                     }
-                    movementList.Add("Movement List printed | " + DateTime.Now);
+                    movementList.Add(">> Movement List printed | " + DateTime.Now);
                     break;
 
                 case 4:
-                    Console.WriteLine("These are all your incomes:\n");
+                    Console.WriteLine("=================================================\n" +
+                        "These are all your incomes:\n" +
+                        "-------------------------------------------------");
 
                     foreach (string movement in movementList)
                     {
                         if (movement.StartsWith('+')) Console.WriteLine(movement);
                     }
-                    movementList.Add("Income List printed | " + DateTime.Now);
+                    movementList.Add(">> Income List printed | " + DateTime.Now);
                     break;
 
                 case 5:
-                    Console.WriteLine("These are all your incomes:\n");
+                    Console.WriteLine("=================================================\n" +
+                        "These are all your outcomes:\n" +
+                        "-------------------------------------------------");
 
                     foreach (string movement in movementList)
                     {
                         if (movement.StartsWith('-')) Console.WriteLine(movement);
                     }
-                    movementList.Add("Outcome List printed | " + DateTime.Now);
+                    movementList.Add(">> Outcome List printed | " + DateTime.Now);
                     break;
 
                 case 6:
-                    Console.WriteLine($"Current money: {currentMoney:0.00} €");
-                    movementList.Add("Current Money consulted | " + DateTime.Now);
+                    Console.WriteLine($"Current money: {currentMoney:0.00}€");
+                    movementList.Add(">> Account Money consulted | " + DateTime.Now);
                     break;
 
             }
 
             Console.WriteLine("Do you wish to make another operation?\n" +
-                "Write yes/no: ");
+                "Write 'yes' if so or type anything else to exit: ");
+            Console.Write("=> ");
             input = Console.ReadLine()?.Trim();
             if (input != null && input.Length > 0 && !input.ToLower().Equals("yes"))
             {
@@ -256,7 +271,7 @@ do
         else
         {
             exit = true;
-            Console.WriteLine($"Current money: {currentMoney:0.00} €");
+            Console.WriteLine($"Current money: {currentMoney:0.00}€");
             Console.WriteLine("Thanks for using our services. Goodbye!");
         }
     }
